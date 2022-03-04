3f50090454886e9f7e1fb10812304138d8b4969c
ErickBss <erickbasso22@gmail.com>
2022-03-04 18:14:34 -0300

    main content

--- src/components/signInButton/index.tsx
+++ src/components/signInButton/index.tsx
@@ -1,11 +1,18 @@
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
 
 import styles from './style.module.scss'
 
 export function SignInButton() {
   const isUserLoggedIn = true
 
-  return (
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      BssErick
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
     <button className={styles.signInButton}>
       <FaGithub color="#eba417" />
       Sign In with Git Hub
